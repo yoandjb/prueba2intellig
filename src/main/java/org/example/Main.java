e4aa51569a3317fe8bc44b99b856508a95973582
Joan Castillo <yoan_c.b@hotmail.com>
2023-08-24 19:51:19 -0400

    creando rama trabajo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args)
     {
-        System.out.println("inicializando");
-        System.out.println("dependecia1");
+        System.out.println("inicializando solo en rama trabajo");
+
     }
 }
